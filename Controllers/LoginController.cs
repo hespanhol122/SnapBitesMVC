d5bf2a9521660256186204d709b0a978646c65b3
Vinicius Hespanhol <53355940+hespanhol122@users.noreply.github.com>
2023-11-23 11:08:38 -0300

    Foi adicionado cadastro e login telas

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SnapBites.Models;
+using System.Diagnostics;
+
+namespace SnapBites.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public LoginController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
