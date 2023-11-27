ff01864d6aa5a9e4145ed19b6dd09962d007d8fb
Vinicius Hespanhol <53355940+hespanhol122@users.noreply.github.com>
2023-11-27 06:39:55 -0300

    Add telas

---
+++ Controllers/FeedController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SnapBites.Models;
+using System.Diagnostics;
+
+namespace SnapBites.Controllers
+{
+    public class FeedController : Controller
+    {
+        private readonly ILogger<FeedController> _logger;
+
+        public FeedController(ILogger<FeedController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Feed()
+        {
+            return View();
+        }
+
+        public IActionResult FeedPg2()
+        {
+            return View();
+        }
+
+        public IActionResult FeedPg3()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -23,6 +23,12 @@ namespace SnapBites.Controllers
             return View();
         }
 
+        public IActionResult Interesses() 
+        {
+            return View();
+        }
+            
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
